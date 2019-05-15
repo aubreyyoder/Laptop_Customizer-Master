5058a18a1e440ed582aeddbdf2d626f77c8861eb
Aubrey Yoder <Aubreyyoder@gmail.com>
2019-05-15 12:34:08 -0700

    Added Components

---
+++ src/SummaryOption/SummaryOption.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./SummaryOption.css";
+
+class SummaryOption extends React.Component {
+  render() {
+    return <div className="summary" />;
+  }
+}
+
+export default SummaryOption;
---
+++ src/TitleBar/TitleBar.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./TitleBar.css";
+
+class TitleBar extends React.Component {
+  render() {
+    return (
+      <div className="TitleBar">
+        <div className="TitleBar__heading">
+          <h1>ELF Computing</h1>
+          <h3>Laptops</h3>
+          <h5>Customize your laptop</h5>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default TitleBar;
